915266a6e7ec77155dcbff11a3185cc41ab3b8cf
Sibongudumo Ntombela <sintombe@c4r18s2.wethinkcode.co.za>
2020-07-15 11:41:40 +0200

    group

---
+++ ex00/main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "rush00.c"
+#include "rush01.c"
+#include "rush02.c"
+#include "rush03.c"
+#include "rush04.c"
+
+int main()
+{
+  rush( 5, 5);
+
+  return (0);
+}
---
+++ ex00/rush02.c
@@ -0,0 +1,53 @@
+
+#include <unistd.h>
+#include <ft_putchar.c>
+
+void print(int x, char start, char mid, char end)
+{
+  int i;
+  i = 1;
+  while ( i <= x)
+  {
+    if (i==1)
+    {
+      ft_putchar(start);
+    }
+    else if (i == x)
+    {
+      ft_putchar(end);
+    }
+    else
+    {
+      ft_putchar(mid);
+    }
+    i++;
+  }
+  ft_putchar('\n');
+  
+}
+
+void rush(int x, int y)
+{
+      int i = 1;
+       while (i <= y)
+    {
+      if (i == 1)
+      {
+        print(x, 'A', 'B', 'A' );
+      }
+      else if (i == y)
+      {
+       print(x, 'B', ' ' ,'B');
+      }
+      else
+      {
+        
+         print(x, 'C', 'B', 'C');
+      }
+      i++;
+    }
+  
+  
+}
+
+
